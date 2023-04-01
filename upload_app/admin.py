b8d8c7defdd656d1e9099ff205f19af0c361aa8d
joberthdavid <joberthdavid@hotmail.com>
2023-04-01 17:15:15 -0300

    Retornando admin sem actions

--- upload_app/admin.py
+++ upload_app/admin.py
@@ -3,7 +3,7 @@ from upload_app.models import ModelFileCost
 from .usefuls.processing_file import processing_file
 
 
-class ModelFileCostAdmin(admin.ModelAdmin):
-    actions = [processing_file,]
+# class ModelFileCostAdmin(admin.ModelAdmin):
+#     actions = [processing_file,]
 
-admin.site.register(ModelFileCost, ModelFileCostAdmin)
+admin.site.register(ModelFileCost)#, ModelFileCostAdmin)
