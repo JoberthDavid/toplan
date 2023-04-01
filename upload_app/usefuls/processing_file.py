75c9298aec01710504bd0546c8d0b62994a7c8d1
joberthdavid <joberthdavid@hotmail.com>
2023-04-01 17:22:44 -0300

    Remoção da função de actions

--- upload_app/usefuls/processing_file.py
+++ upload_app/usefuls/processing_file.py
@@ -1,21 +1,21 @@
-import pdftotext
-from upload_app.models import ModelFileCost
+# import pdftotext
+# from upload_app.models import ModelFileCost
 
 
-def processing_file(modeladmin, request, queryset):
-    queryset = ModelFileCost.objects.filter(status=False)
-    for file_in_field in queryset:
+# def processing_file(modeladmin, request, queryset):
+#     queryset = ModelFileCost.objects.filter(status=False)
+#     for file_in_field in queryset:
         
-        if file_in_field.type_file == "AN":
-            path = file_in_field.file
+#         if file_in_field.type_file == "AN":
+#             path = file_in_field.file
 
-            with path.open(mode="rb") as f:
-                pdf = pdftotext.PDF(f)
-                num_pages = len(pdf)
+#             with path.open(mode="rb") as f:
+#                 pdf = pdftotext.PDF(f)
+#                 num_pages = len(pdf)
                 
-                print(type(pdf))
-                print(num_pages)
-        else:
-            print("*** Arquivo errado ***")
+#                 print(type(pdf))
+#                 print(num_pages)
+#         else:
+#             print("*** Arquivo errado ***")
 
-processing_file.short_description = 'Processar arquivo'
+# processing_file.short_description = 'Processar arquivo'
