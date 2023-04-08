0b1afbe82987d00bff84e64ac389ddb0ee9514f3
joberthdavid <joberthdavid@hotmail.com>
2023-04-08 09:41:42 -0300

    Alteração da biblioteca de leitura pdf

--- upload_app/usefuls/processing_file.py
+++ upload_app/usefuls/processing_file.py
@@ -1,4 +1,4 @@
-import pdftotext
+import PyPDF2
 from upload_app.models import ModelFileCost
 
 
@@ -10,10 +10,29 @@ def processing_file(modeladmin, request, queryset):
             path = file_in_field.file
 
             with path.open(mode="rb") as f:
-                pdf = pdftotext.PDF(f)
-                num_pages = len(pdf)
-                print(num_pages)
+                reader = PyPDF2.PdfReader(f)
+                print(len(reader.pages))
+                print(reader.pages[0].extract_text())
         else:
             pass
+
+
+
+
+# import pdftotext
+# def processing_file(modeladmin, request, queryset):
+#     queryset = ModelFileCost.objects.filter(status=False)
+#     for file_in_field in queryset:
+        
+#         if file_in_field.type_file == "AN":
+#             path = file_in_field.file
+
+#             with path.open(mode="rb") as f:
+#                 pdf = pdftotext.PDF(f)
+#                 num_pages = len(pdf)
+#                 print(num_pages)
+#                 print(pdf[0])
+#         else:
+#             pass
 
 processing_file.short_description = 'Processar arquivo'
